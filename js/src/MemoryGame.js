79fdec06083b4cc2eeb6188b0c9379e2b66c05bc
valesil <sil.valencia@gmail.com>
2014-10-16 02:01:50 -0400

    Update index.html and MemoryGame.js

--- js/src/MemoryGame.js
+++ js/src/MemoryGame.js
@@ -6,70 +6,95 @@ var game = function () {
     var matrix = [];
     var displayMatrix = [];
     var size = 0;
+    
     var getSize = function (){
         do{
-            size = parseInt(prompt ("Introduce the matrix size"));
+            size = parseInt(prompt ('Introduce the matrix size'));
         }while (size % 2 > 0);
         return size;
     }
 
     var createMatrix = function (){
-    for (i = 0; i < 3; i++){
-    matriz[i]=new Array(3);
-    }
+        //create the matrix empty
+        for (i = 0; i < size; i++){
+            matrix[i]=[];
+        }
 
-
-        for (i = 0; i < size/2; i++){     //to fill all the array
+        //populate values randomly
+        var x = 0;
+        var y = 0;
+        for (i = 0; i < size*size/2; i++){
             j = 0;
-            while (j < 2){    //to fill twice
-                var pos = parseInt(Math.random() * size);
-                while (matrix[pos] == undefined)  //to fill in an empty cell
-                {
-                    matrix[pos] = String.fromCharCode(i + 65);
-                    pos = parseInt(Math.random() * size);
+            do {    //to fill twice
+                x = parseInt(Math.random() * size);
+                y = parseInt(Math.random() * size);
+                if (matrix[x][y] == undefined) {
+                    matrix[x][y] = String.fromCharCode(i + 65);
                     j++;
                 };
-            }
+            } while (j < 2)
         }
     }
 
     var createDisplayMatrix = function (){
         for (i = 0; i < size; i++){
-            displayMatrix[i] = 'X';
+            displayMatrix[i]=[];
+            for (j = 0; j < size; j++){
+                displayMatrix[i][j] = 'X';
+            }
         }
     }
 
     var showMatrix = function (matrix){
         //to dispaly the indexes
-        var indexes = '';
-        for (i = 1; i <= size; i++) {
-            indexes = indexes + ' ' + i.toString();
+        var indexes = ' ';
+        for (i = 0; i < size; i++) {
+            indexes = indexes + ' ' + i;
         };
         console.log(indexes);
 
-        var values = '';
-        for (i = 0; i < size; i++) {
-            values = values + ' ' + matrix[i];
-        };
-        console.log(values);
+        for (i = 0; i < size; i++){
+            console.log(i + ' ' + matrix[i].toString());
+        }
     }
 
     var play = function (){
         do{
-            var firstPos = parseInt(prompt("Introduce the first guest"));
-            var secondPos = parseInt(prompt("Introduce the second guest"));
+            var firstGuest = prompt('Introduce the first guest in the following format "row,col"');
+            var firstRow = parseInt(firstGuest.substring(0,1));
+            var firstCol = parseInt(firstGuest.substring(2,3));
+            displayMatrix[firstRow][firstCol] = matrix[firstRow][firstCol];
+            showMatrix(displayMatrix);
 
-            if (matrix[firstPos] == matrix[secondPos]){
-                displayMatrix[firstPos] = matrix[firstPos];
-                displayMatrix[secondPos] = matrix[secondPos];
-                showMatrix(displayMatrix);
+            var secondGuest = prompt('Introduce the second guest in the following format "row,col"');
+            var secondRow = parseInt(secondGuest.substring(0,1));
+            var secondCol = parseInt(secondGuest.substring(2,3));
+            displayMatrix[secondRow][secondCol] = matrix[secondRow][secondCol];
+            showMatrix(displayMatrix);
+
+
+            if (matrix[firstRow][firstCol] != matrix[secondRow][secondCol]){
+                displayMatrix[firstRow][firstCol] = 'X';
+                displayMatrix[secondRow][secondCol] = 'X';
+                console.log('You did not guess the pair');
+                //showMatrix(displayMatrix);
             }
-        } while (displayMatrix.indexOf('X') >= 0)
+            else{
+                console.log('You guessed the pair');
+            }
+            showMatrix(displayMatrix);
+        //} while (displayMatrix.indexOf('X') >= 0)
+        }while (matrix.toString() != displayMatrix.toString())
+        alert('You have finished the game!!');
     }
 
     getSize();
     createMatrix();
+    createDisplayMatrix();
+    console.log('The matrix with values is:')
     showMatrix(matrix);
-
+    console.log('The matrix for the game is:')
+    showMatrix(displayMatrix);
+    play();
 }
 
